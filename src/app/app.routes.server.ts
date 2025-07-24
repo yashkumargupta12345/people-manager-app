342babe36efb5334b8f9eccd4b04707b71d48c8b
yashkumargupta12345 <guptayashyg45227@gmail.com>
2025-07-24 16:02:26 +0530

    fixed deployment issues

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'create',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'delete/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
